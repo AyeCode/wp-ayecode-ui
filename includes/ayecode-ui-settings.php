4bb5b0c46c494155ba402c379dc7a50cfb7b707e
stiofan <info@wpgeodirectory.com>
2021-05-11 14:24:16 +0100

    0.1.47

--- includes/ayecode-ui-settings.php
+++ includes/ayecode-ui-settings.php
@@ -35,7 +35,7 @@ if ( ! class_exists( 'AyeCode_UI_Settings' ) ) {
 		 *
 		 * @var string
 		 */
-		public $version = '0.1.46';
+		public $version = '0.1.47';
 
 		/**
 		 * Class textdomain.
@@ -984,13 +984,15 @@ if ( ! class_exists( 'AyeCode_UI_Settings' ) ) {
 			<?php
 			$output = ob_get_clean();
 
+
+
 			/*
 			 * We only add the <script> tags for code highlighting, so we strip them from the output.
 			 */
 			return str_replace( array(
 				'<script>',
 				'</script>'
-			), '', $output );
+			), '', self::minify_js($output) );
 		}
 
 
@@ -1364,7 +1366,7 @@ if ( ! class_exists( 'AyeCode_UI_Settings' ) ) {
 
 			<?php if( defined( 'SVQ_THEME_VERSION' ) ){ ?>
 			/* KLEO theme specific */
-			.kleo-main-header .navbar-collapse.collapse.show:not(.in){display: inherit !important;}
+			.kleo-main-header .navbar-collapse.collapse.show:not(.in){display: block !important;}
 			<?php } ?>
 
 			<?php if( defined( 'FUSION_BUILDER_VERSION' ) ){ ?>
@@ -1378,7 +1380,7 @@ if ( ! class_exists( 'AyeCode_UI_Settings' ) ) {
 			return str_replace( array(
 				'<style>',
 				'</style>'
-			), '', ob_get_clean());
+			), '', self::minify_css( ob_get_clean() ) );
 		}
 
 
@@ -1420,7 +1422,7 @@ if ( ! class_exists( 'AyeCode_UI_Settings' ) ) {
 			return str_replace( array(
 				'<style>',
 				'</style>'
-			), '', ob_get_clean());
+			), '', self::minify_css( ob_get_clean() ) );
 		}
 
 		/**
@@ -2081,6 +2083,87 @@ if ( 0 ) { ?><script><?php } ?>
 			$locale = json_encode( $params );
 
 			return apply_filters( 'ayecode_ui_timeago_locale', trim( $locale ) );
+		}
+
+		/**
+		 * JavaScript Minifier
+		 *
+		 * @param $input
+		 *
+		 * @return mixed
+		 */
+		public static function minify_js($input) {
+			if(trim($input) === "") return $input;
+			return preg_replace(
+				array(
+					// Remove comment(s)
+					'#\s*("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')\s*|\s*\/\*(?!\!|@cc_on)(?>[\s\S]*?\*\/)\s*|\s*(?<![\:\=])\/\/.*(?=[\n\r]|$)|^\s*|\s*$#',
+					// Remove white-space(s) outside the string and regex
+					'#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\'|\/\*(?>.*?\*\/)|\/(?!\/)[^\n\r]*?\/(?=[\s.,;]|[gimuy]|$))|\s*([!%&*\(\)\-=+\[\]\{\}|;:,.<>?\/])\s*#s',
+					// Remove the last semicolon
+					'#;+\}#',
+					// Minify object attribute(s) except JSON attribute(s). From `{'foo':'bar'}` to `{foo:'bar'}`
+					'#([\{,])([\'])(\d+|[a-z_][a-z0-9_]*)\2(?=\:)#i',
+					// --ibid. From `foo['bar']` to `foo.bar`
+					'#([a-z0-9_\)\]])\[([\'"])([a-z_][a-z0-9_]*)\2\]#i'
+				),
+				array(
+					'$1',
+					'$1$2',
+					'}',
+					'$1$3',
+					'$1.$3'
+				),
+				$input);
+		}
+
+		/**
+		 * Minify CSS
+		 *
+		 * @param $input
+		 *
+		 * @return mixed
+		 */
+		public static function minify_css($input) {
+			if(trim($input) === "") return $input;
+			return preg_replace(
+				array(
+					// Remove comment(s)
+					'#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')|\/\*(?!\!)(?>.*?\*\/)|^\s*|\s*$#s',
+					// Remove unused white-space(s)
+					'#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\'|\/\*(?>.*?\*\/))|\s*+;\s*+(})\s*+|\s*+([*$~^|]?+=|[{};,>~]|\s(?![0-9\.])|!important\b)\s*+|([[(:])\s++|\s++([])])|\s++(:)\s*+(?!(?>[^{}"\']++|"(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')*+{)|^\s++|\s++\z|(\s)\s+#si',
+					// Replace `0(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)` with `0`
+					'#(?<=[\s:])(0)(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)#si',
+					// Replace `:0 0 0 0` with `:0`
+					'#:(0\s+0|0\s+0\s+0\s+0)(?=[;\}]|\!important)#i',
+					// Replace `background-position:0` with `background-position:0 0`
+					'#(background-position):0(?=[;\}])#si',
+					// Replace `0.6` with `.6`, but only when preceded by `:`, `,`, `-` or a white-space
+					'#(?<=[\s:,\-])0+\.(\d+)#s',
+					// Minify string value
+					'#(\/\*(?>.*?\*\/))|(?<!content\:)([\'"])([a-z_][a-z0-9\-_]*?)\2(?=[\s\{\}\];,])#si',
+					'#(\/\*(?>.*?\*\/))|(\burl\()([\'"])([^\s]+?)\3(\))#si',
+					// Minify HEX color code
+					'#(?<=[\s:,\-]\#)([a-f0-6]+)\1([a-f0-6]+)\2([a-f0-6]+)\3#i',
+					// Replace `(border|outline):none` with `(border|outline):0`
+					'#(?<=[\{;])(border|outline):none(?=[;\}\!])#',
+					// Remove empty selector(s)
+					'#(\/\*(?>.*?\*\/))|(^|[\{\}])(?:[^\s\{\}]+)\{\}#s'
+				),
+				array(
+					'$1',
+					'$1$2$3$4$5$6$7',
+					'$1',
+					':0',
+					'$1:0 0',
+					'.$1',
+					'$1$3',
+					'$1$2$4$5',
+					'$1$2$3',
+					'$1:0',
+					'$1$2'
+				),
+				$input);
 		}
 	}
 
